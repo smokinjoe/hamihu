56aec0740dba95b438adb54bcf2cc2b707ab6d9a
Joe <joe.ekiert@gmail.com>
2014-03-27 05:16:41 +0000

    initial commit in rails app branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamihu::Application.config.secret_key_base = 'b8ea7e33790fec84de579a8179ebf17a5e7101357167d19cdb9c7f97e96cc26b04c17c999b6e6baebdd62deaeac0ba93dcb7af404b7580b55f6fd78def6761d7'
