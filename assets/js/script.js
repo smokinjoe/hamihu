7d19d90dbff6787c24f3e3db506e02eb0752b7d7
Joe Ekiert <jekiert@helixeducation.com>
2014-11-07 23:43:34 -0800

    slowly making headway into adding stats to players

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,57 @@ var app = (function () {
   // privates
   var players = [],
       playerIndex = 1;
-
   var stats = [],
       statIndex = 1;
 
+  // Stat class stuff
   var Stat = function (stat) {
-    this.id = statIndex++;
-    this.class = "Stat";
+    if (stat) {
+      this.id = statIndex++;
+      this.class = "Stat";
+
+      this.name = stat.name;
+      stats.push(this);
+    }
+    else {
+      return {
+        get : function (id) {
+          return stats.filter(function (s) {
+            return s.id === id;
+          });
+        }
+      };
+    }
+  };
+
+  Stat.prototype.destroy = function () {
+    var self = this;
+    return _.remove(stat, function (s) {
+      return s.id === self.id;
+    });
+  };
+
+  Stat.prototype.update = function (data) {
+    var self = this;
+    self.name = data.name ? data.name : self.name;
+    return self;
+  };
+
+  Stat.prototype.createPlayerStat = function (value) {
+    var stat = new PlayerStat(this);
+    stat.updateValue(value);
+    return stat;
+  };
+
+  // Player Stat
+  var PlayerStat = function (stat) {
+    this.class = "PlayerStat";
+    this.stat_id = stat.id;
     this.name = stat.name;
-    this.value = stat.value;
-    stats.push(this);
+  };
+
+  PlayerStat.prototype.updateValue = function (value) {
+    this.value = value;
   };
 
   // Player class stuff
@@ -19,7 +60,7 @@ var app = (function () {
     if (player) {
       this.id = playerIndex++;
       this.class = "Player";
-      
+
       this.name = player.name;
       this.stats = [];
       players.push(this);
@@ -27,25 +68,31 @@ var app = (function () {
     else {
       return {
         get : function (id) {
-          return players.filter(function (p) {
+          var player = players.filter(function (p) {
             return p.id === id;
           });
+          return player[0];
         }
       };
     }
   };
 
   Player.prototype.destroy = function () {
+    var self = this;
+    return _.remove(players, function (p) {
+      return p.id === self.id;
+    });
   };
 
-  Player.prototype.addStat = function (stat) {
+  Player.prototype.update = function (data) {
     var self = this;
-    // add from main array
+    self.name = data.name ? data.name : self.name;
+    self.stats = data.stats ? data.stats : self.stats;
     return self;
   };
 
   return {
-    addPlayer : function (player) {
+    createPlayer : function (player) {
       return new Player(player);
     },
     getPlayer : function (id) {
@@ -56,7 +103,21 @@ var app = (function () {
     },
     allPlayers : function () {
       return players;
+    },
+
+    createStat : function (stat) {
+      return new Stat(stat);
+    },
+    getStat : function (id) {
+      return Stat().get(id);
+    },
+    removeStat : function (id) {
+      return Stat().get(id).destroy();
+    },
+    allStats : function () {
+      return stats;
     }
+
   };
 
 }());
