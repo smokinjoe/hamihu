c009421810c2a355dbe30ed2de760db948f86a4d
Joe Ekiert <jekiert@helixeducation.com>
2014-11-08 19:20:19 -0800

    finished writing up promise.js - now to see if it works!

--- assets/js/promise.js
+++ assets/js/promise.js
@@ -102,102 +102,117 @@
     return result;
   }
 
+  function new_xhr () {
+    var xhr;
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject) {
+      try {
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+      }
+      catch (e) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+    }
+    return xhr;
+  }
 
+  function ajax (method, url, data, headers) {
+    var p = new Promise();
+    var xhr, payload;
+    data = data || {};
+    headers = headers || {};
 
+    try {
+      xhr = new_xhr();
+    }
+    catch (e) {
+      p.done(promise.ENOXHR, "");
+      return p;
+    }
 
+    payload = _encode(data);
+    if (method === 'GET' && payload) {
+      url += '?' + payload;
+      payload = null;
+    }
 
+    xhr.open(method, url);
+    xhr.setRequestHeader('Content-type',
+                         'application/x-www-form-urlencoded');
+    for (var h in headers) {
+      if (headers.hasOwnProperty(h)) {
+        xhr.setRequestHeader(h, headers[h]);
+      }
+    }
 
+    function onTimeOut() {
+      xhr.abort();
+      p.done(promise.ETIMEOUT, "", xhr);
+    }
 
+    var timeout = promise.ajaxTimeout;
+    if (timeout) {
+      var tid = setTimeout(onTimeout, timeout);
+    }
 
+    xhur.onreadystatechange = function () {
+      if (timeout) {
+        clearTimeout(tid);
+      }
+      if (xhr.readyState === 4) {
+        var err = (!xhr.status ||
+                    (xhr.status < 200 || xhr.status >= 300) &&
+                    xhr.status !== 304);
+        p.done(err, xhr.responseText, xhr);
+      }
+    };
+
+    xhr.send(payload);
+    return p;
+  }
+
+  function _ajaxer(method) {
+    return function (url, data, headers) {
+      return ajax(method, url, data, headers);
+    };
+  }
+
+  var promise = {
+    Promise: Promise,
+    join: join,
+    chain: chain,
+    ajax: ajax,
+    get: _ajaxer('GET'),
+    post: _ajaxer('POST'),
+    put: _ajaxer('PUT'),
+    del: _ajaxer('DELETE'),
+
+    /* Error codes */
+    ENOXHR: 1,
+    ETIMEOUT: 2,
+
+    /**
+     * Configuration parameter: time in milliseconds after which a
+     * pending AJAX request is considered unresponsive and is
+     * aborted. Useful to deal with bad connectivity (e.g. on a
+     * mobile network). A 0 value disables AJAX timeouts.
+     *
+     * Aborted requests resolve the promise with a ETIMEOUT error
+     * code
+     */
+    ajaxTimeout: 0
+  };
+
+  if (typeof define === 'function' && define.amd) {
+    /* AMD support */
+    define(function () {
+      return promise;
+    });
+  }
+  else {
+    exports.promise = promise;
+  }
 
 }(this));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
