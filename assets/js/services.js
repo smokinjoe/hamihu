84aae376942f331c5ebbf9f0dce700d0227790b2
Joe Ekiert <jekiert@helixeducation.com>
2014-11-19 19:36:18 -0800

    creating Stat service

--- assets/js/services.js
+++ assets/js/services.js
@@ -8,13 +8,27 @@
    * Player
    */
   angular.module('hamihu.services').
-    factory('Player', [function() {
+    factory('Player', [function () {
       var Player = function (name) {
         this.name = name;
         this.stats = [];
       };
 
       return Player;
+    }]);
+
+
+  /*
+   * Stats
+   */
+  angular.module('hamihu.services').
+    factory('Stat', [function () {
+      var Stat = function (name) {
+        this.name = name;
+        this.value = 0;
+      };
+
+      return Stat;
     }]);
 
 }());
